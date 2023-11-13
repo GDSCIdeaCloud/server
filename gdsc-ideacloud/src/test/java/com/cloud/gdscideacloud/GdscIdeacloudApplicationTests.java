d7617a60f619e35b17c8e05465c954c3e73786dc
seonghwa95 <holyflower95@naver.com>
2023-11-13 15:27:52 +0900

    project 생성

---
+++ gdsc-ideacloud/src/test/java/com/cloud/gdscideacloud/GdscIdeacloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.gdscideacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscIdeacloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
